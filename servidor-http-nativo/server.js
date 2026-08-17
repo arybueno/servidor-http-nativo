9c067abf5f4064d27859e9604c3579574ec803c5
Aryane <aryanebueno70@gmail.com>
2026-08-17 20:28:58 +0000

    edit: alteração do content-type e retorno

--- servidor-http-nativo/server.js
+++ servidor-http-nativo/server.js
@@ -4,12 +4,12 @@ const PORTA = 3000
 
 const server = http.createServer((req, res) => {
     console.log(`Requisição recebida! ${req.method} ${req.url}`);
-    console.log(new Date().toISOString();)
+    console.log(new Date().toISOString())
 
     res.statusCode = 201;
-    res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+    res.setHeader('Content-Type', 'application/json; charset=utf-8');
 
-    res.end("Recurso criado");
+    res.end(JSON.stringify({ status: "ok" }));
 });
 
 server.listen(PORTA, () => {
