8a5b99e17d6a46378fb9a1160d93c41fa31fb363
Aryane <aryanebueno70@gmail.com>
2026-08-17 20:25:37 +0000

    edit: hora da req

--- servidor-http-nativo/server.js
+++ servidor-http-nativo/server.js
@@ -4,6 +4,7 @@ const PORTA = 3000
 
 const server = http.createServer((req, res) => {
     console.log(`Requisição recebida! ${req.method} ${req.url}`);
+    console.log(new Date().toISOString();)
 
     res.statusCode = 201;
     res.setHeader('Content-Type', 'text/plain; charset=utf-8');
